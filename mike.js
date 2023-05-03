7eaf275fc3341badc518698ae7815f02c51c233c
dozie <techprofessor26@gmail.com>
2023-05-03 14:52:30 -0700

    initial commit

---
+++ mike.js
@@ -0,0 +1,11 @@
+
+var firstNumber = prompt("Please provide name"); 
+var secondNumber =prompt(); 
+alert(Text(firstNumber) + Number(secondNumber));
+//
+
+if (Text(text) < 5) {
+	alert("hello");
+} else if (Text(text) > 5) {
+	alert("your under age ");
+}
